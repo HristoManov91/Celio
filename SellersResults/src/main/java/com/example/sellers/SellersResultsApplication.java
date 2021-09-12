622d7d7eeda628f88bd335e2d71d34bbd64f3171
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-09-12 23:40:01 +0300

    start the project

---
+++ SellersResults/src/main/java/com/example/sellers/SellersResultsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersResultsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellersResultsApplication.class, args);
+    }
+
+}
